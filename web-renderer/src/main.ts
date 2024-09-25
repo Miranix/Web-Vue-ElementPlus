a143cc04718396d53c9ada3cc3e97c58764235e4
Daimi <wzt252951@163.com>
2024-09-25 09:38:19 +0800

    add website logo and title

--- web-renderer/src/main.ts
+++ web-renderer/src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import packageJson  from '../../package.json'
+
+document.title = packageJson.name;
+
 
 const app = createApp(App)
-
 app.use(ElementPlus)
 app.mount('#app')
